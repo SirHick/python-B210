6d6c8c7a00fb4a48f1186de63c02e66aadd647a2
Henrique Miguel Deschamps Dahmer <henriquedahmer1234@gmail.com>
2026-06-16 09:24:55 -0300

    Comecei Python com o Prof novo

---
+++ PythonProject/dicionarioDados.py
@@ -0,0 +1,41 @@
+idade = 18
+
+nome = "Henrique"
+
+altura = 1.76
+
+aprovado = True
+
+frutas = ("Maçã", "Banana", "Uva")
+
+cores = ["Azul", "Verde", "Vermelho"]
+
+aluno = {
+    "nome": "Diego",
+    "idade": 18,
+    "curso": "Engenharia de Software"
+}
+
+
+print("Exemplos de Tipos de Dados em Python\n")
+
+print(f"Nome: {nome}")
+print(f"Tipo: {type(nome)}\n")
+
+print(f"Idade: {idade}")
+print(f"Tipo: {type(idade)}\n")
+
+print(f"Altura: {altura}")
+print(f"Tipo: {type(altura)}\n")
+
+print(f"Aprovado: {aprovado}")
+print(f"Tipo: {type(aprovado)}\n")
+
+print(f"Frutas: {frutas}")
+print(f"Tipo: {type(frutas)}\n")
+
+print(f"Cores: {cores}")
+print(f"Tipo: {type(cores)}\n")
+
+print(f"Aluno: {aluno}")
+print(f"Tipo: {type(aluno)}\n")
---
+++ PythonProject/exemplo1.py
@@ -0,0 +1,3 @@
+idade = int(input("Digite sua idade: "))
+
+print(f"Sua idade ({idade}) daqui a 5 anos será de: {idade + 5} anos ")
